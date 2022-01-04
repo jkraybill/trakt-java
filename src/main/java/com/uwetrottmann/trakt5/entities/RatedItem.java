2dff32ff4d27e3ce2ff81abe40b936bc4bbc13ae
jkraybill <jeremy@dashcord.com>
2022-01-04 23:25:44 +1100

    added support for all ratings

---
+++ src/main/java/com/uwetrottmann/trakt5/entities/RatedItem.java
@@ -0,0 +1,16 @@
+package com.uwetrottmann.trakt5.entities;
+
+/**
+ * A rating - can contain a show, a season, an episode, or a movie.
+ */
+public class RatedItem extends BaseRatedEntity {
+
+	/** movie / show / season / episode */
+	public String type;
+
+	public Show show;
+	public Episode episode;
+	public Season season;
+	public Movie movie;
+
+}
